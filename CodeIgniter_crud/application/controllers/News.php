900a6515b53492b367b64350e03638a4d1792677
髙橋剛志 <tt2123244@gmail.com>
2021-06-30 23:48:35 +0900

    CRUD完成

--- CodeIgniter_crud/application/controllers/News.php
+++ CodeIgniter_crud/application/controllers/News.php
@@ -73,5 +73,53 @@ class News extends CI_Controller {
                 $this->load->view('templates/footer');
             }
         }
+        public function edit()
+        {
+            $id = $this->uri->segment(3);
+    
+            if (empty($id))
+            {
+                show_404();
+            }
+            
+            $this->load->helper('form');
+            $this->load->library('form_validation');
+            
+            $data['title'] = 'Edit a news item';        
+            $data['news_item'] = $this->news_model->get_news_by_id($id);
+            
+            $this->form_validation->set_rules('title', 'Title', 'required');
+            $this->form_validation->set_rules('text', 'Text', 'required');
+     
+            if ($this->form_validation->run() === FALSE)
+            {
+                $this->load->view('templates/header', $data);
+                $this->load->view('news/edit', $data);
+                $this->load->view('templates/footer');
+     
+            }
+            else
+            {
+                $this->news_model->set_news($id);
+                $this->load->view('news/success');
+                redirect( base_url() . 'index.php/news');
+            }
+        }
+        
+        public function delete()
+        {
+            $id = $this->uri->segment(3);
+            
+            if (empty($id))
+            {
+                show_404();
+            }
+                    
+            $news_item = $this->news_model->get_news_by_id($id);
+            
+            $this->news_model->delete_news($id);        
+            redirect( base_url() . 'index.php/news');        
+        }
+    
     
 }
